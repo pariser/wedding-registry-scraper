989e75c472ed0ec5fac943593dc0f474a843583f
Andrew Pariser <pariser@gmail.com>
2015-02-23 23:27:19 -0800

    Change zola to public registry

--- registries/zola.rb
+++ registries/zola.rb
@@ -1,31 +1,7 @@
 #!/usr/bin/env ruby
 
-class Registries::Zola < PrivateRegistry
-  @login_url = 'https://www.zola.com/account/login'
-  @login_cookies = [
-    'guid'
-  ]
-
-  def initialize(params={})
-    params.symbolize_keys!
-
-    redirect_url = params[:url].sub(/^https?:\/\/www\.zola\.com/, '')
-
-    params.merge!(:login_params => {
-      :email => params.delete(:email),
-      :password => params.delete(:password),
-      :redirect_url => redirect_url,
-    })
-
-    super(params)
-  end
-
+class Registries::Zola < PublicRegistry
 private
-
-  def authenticated?(doc)
-    doc.css('head title').text.strip != "Login on Zola"
-  end
-
   def get_products(doc)
     doc.css('#all-panel .product-tile')
   end
