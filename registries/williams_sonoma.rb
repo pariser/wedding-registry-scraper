d259b75b216f34e7c7f9f92e45a3e15529d06167
Andrew Pariser <pariser@gmail.com>
2015-02-23 23:17:03 -0800

    Add Williams-Sonoma

---
+++ registries/williams_sonoma.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+class Registries::WilliamsSonoma < PublicRegistry
+private
+  def get_products(doc)
+    doc.css('table.registry-category-list tbody tr')
+  end
+
+  def get_name(product)
+    product.css('.product-detail .product-info .title a').text.strip
+  end
+
+  def get_sku(product)
+    sku = product.css('.product-detail .product-info .item-number').text.strip.match(/: (\d+)/)[1]
+    "williams-sonoma:#{sku}"
+  end
+
+  def get_url(product)
+    product.css('.product-detail .product-info .title a')[0]['href']
+  end
+
+  def get_remaining(product)
+    desired = product.css('td.requested').text.strip.to_i
+    still_needs = product.css('td.still-needs').text.strip.to_i
+    still_needs
+  end
+end
