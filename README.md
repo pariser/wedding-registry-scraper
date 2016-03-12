fa4ae3bef95e41d012ffe3f3ea1d1c8a6282547f
Andrew Pariser <pariser@gmail.com>
2016-03-12 13:30:30 -0800

    Provide an example of generating HTML source using this gem.

--- README.md
+++ README.md
@@ -23,28 +23,56 @@ I intend to get this working with:
 * Sur la Table
 * Pottery Barn
 
-## Usage
+## Getting Raw Registry Data
 
 Add to your Gemfile
 
-    gem 'wedding-registry-scraper'
+```ruby
+gem 'wedding-registry-scraper'
+```
 
 Use `WeddingRegistryScraper#scrape`:
 
-    require 'wedding_registry_scraper'
+```ruby
+require 'wedding_registry_scraper'
 
-    WeddingRegistryScraper.scrape([
-      "http://www.heathceramics.com/giftregistry/view/index/id/YOUR_UNIQUE_REGISTRY_ID",
-      "http://www.crateandbarrel.com/Gift-Registry/YOUR_NAMES/YOUR_UNIQUE_REGISTRY_ID",
-      "https://www.zola.com/registry/YOUR_REGISTRY_SLUG",
-      "http://www.rei.com/GiftRegistryDetails/YOUR_UNIQUE_REGISTRY_ID",
-      "https://secure.williams-sonoma.com/registry/YOUR_UNIQUE_REGISTRY_ID/registry-list.html",
-    ])
+WeddingRegistryScraper.scrape([
+  "http://www.heathceramics.com/giftregistry/view/index/id/YOUR_UNIQUE_REGISTRY_ID",
+  "http://www.crateandbarrel.com/Gift-Registry/YOUR_NAMES/YOUR_UNIQUE_REGISTRY_ID",
+  "https://www.zola.com/registry/YOUR_REGISTRY_SLUG",
+  "http://www.rei.com/GiftRegistryDetails/YOUR_UNIQUE_REGISTRY_ID",
+  "https://secure.williams-sonoma.com/registry/YOUR_UNIQUE_REGISTRY_ID/registry-list.html",
+])
+```
 
+## Example &ndash; Generate HTML Source
+
+The `./example` folder provides a simple example of how how you might
+use the `wedding_registry_scraper` gem to generate html for your wedding
+website's registry.
+
+It uses [Mustache](http://mustache.github.io/) for templating.
+It also relies on [Normalize](https://necolas.github.io/normalize.css/)
+and some custom css to make the generated page look reasonable.
+
+```bash
+# Go to the example folder and install gem dependencies
+cd ./example
+bundle
+
+# Execute the example html generator, `registries.rb`, with your
+# Zola and Williams-Sonoma registries (use your actual URLs),
+bundle exec registries.rb "https://www.zola.com/registry/YOUR_REGISTRY_SLUG" "https://secure.williams-sonoma.com/registry/YOUR_UNIQUE_REGISTRY_ID/registry-list.html" > registries.html
+
+# Open the file
+open registries.html
+```
 
 ## Want to Help?
 
 It should be simple to add another registry by cloning a `Regsitry` inside `lib/wedding_registry_scraper/registries`.
+
+I look forward to your pull-request.
 
 ## Like this project and planning on registering?
 
---
+++ example/registries.rb
@@ -0,0 +1,122 @@
+#!/usr/bin/env ruby
+
+require 'mustache'
+require 'wedding_registry_scraper'
+require 'pry-byebug'
+
+class RegistryNotFoundError < StandardError; end
+
+# A class representing each RegistryItem
+#
+# registry_item_data is a hash that looks like the following:
+# {
+#   name: 'Item Title',
+#   remaining: 1,
+#   desired: 2,
+#   url: 'http://www.some-registry.com/items/123456',
+#   image_url: 'http://www.some-registry.com/items/123456.jpg',
+#   registry_name: 'Some Registry',
+#   fulfilled: true,
+#   price_type: 'Fixed price',
+#   price_value: 90.0,
+# }
+class RegistryItem < Mustache
+  def initialize(sku, registry_item_data)
+    @sku = sku
+    @data = registry_item_data
+  end
+
+  def url
+    @data[:url]
+  end
+
+  def name
+    @data[:name]
+  end
+
+  def image_src
+    @data[:image_url].sub(%r{^(https?:)?//}, 'http://')
+  end
+
+  def fulfillment_description
+    if @data[:fulfilled]
+      "All fulfilled"
+    else
+      "#{@data[:remaining]} of #{@data[:desired]} available"
+    end
+  end
+
+  # Change the price description based on whether this item's "price_type"
+  # is FIXED_PRICE or VARIABLE_PRICE (i.e. contribute what you wish)
+  # @return [Type] description of returned object
+  def price_description
+    case @data[:price_type]
+    when WeddingRegistryScraper::Registry::FIXED_PRICE
+      "$%.2f" % @data[:price_value]
+    when WeddingRegistryScraper::Registry::VARIABLE_PRICE
+      "Contribute what you wish"
+    end
+  end
+end
+
+class Registry < Mustache
+  def initialize(registry_url)
+    @registry_url = registry_url
+
+    @registry_scraper = WeddingRegistryScraper::Registries.initialize_registry(registry_url)
+    unless @registry_scraper
+      raise RegistryNotFoundError, "Could not initialize registry from url #{registry_url.inspect}"
+    end
+
+    STDERR.puts "* Loading items from #{registry_name}"
+    @registry_items = @registry_scraper.get_items.map do |registry_item_sku, registry_item_data|
+      RegistryItem.new(registry_item_sku, registry_item_data)
+    end
+  end
+
+  def registry_url
+    @registry_url
+  end
+
+  def registry_name
+    @registry_scraper.class.display_name
+  end
+
+  def registry_items
+    @registry_items
+  end
+end
+
+# Registries is a Mustache class that renders the `registries.mustache` file
+#
+class Registries < Mustache
+  def initialize(registry_urls)
+    @registries = registry_urls.map do |registry_url|
+      Registry.new(registry_url)
+    end
+  end
+
+  def registries
+    @registries
+  end
+end
+
+# This file is runnable from the command line, supply one or more URLs to
+# scrape as the command line arguments, and redirect output to a file you
+# can open in your browser.
+#
+if $0 == __FILE__
+  if ARGV.length == 0
+    STDERR.puts <<-USAGE
+      Expected to receive one or more registry urls.
+
+      Usage:
+
+        ./registries.rb "http://www.some-registry.com/123456" > registries.html
+
+    USAGE
+    exit 1
+  end
+
+  puts Registries.new(ARGV).render
+end
